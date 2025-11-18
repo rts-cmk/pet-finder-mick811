2ea63289864565b84667b41b63620a1be8306703
mick811 <mikkelelvers@outlook.com>
2025-11-18 09:07:42 +0100

    feat: add listview of the cards

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router"
 import Header from "./ui/Header";
+import ListView from "./ui/listview";
 
 export default function Home() {
     const { dogs, user } = useLoaderData() as {
@@ -10,6 +11,10 @@ export default function Home() {
     return (
         <div className="home">
             <Header image={user.image} location={user.location} />
+
+            <section className="list-view">
+                <ListView dogs={dogs} />
+            </section>
         </div>
     )
 }
