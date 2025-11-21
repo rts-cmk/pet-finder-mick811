fefd044b4017c9cab01a70050d464e250a6e3d3e
mick811 <mikkelelvers@outlook.com>
2025-11-21 08:55:12 +0100

    add errorboundary and hydrate fallback

--- src/main.tsx
+++ src/main.tsx
@@ -7,39 +7,49 @@ import { SettingsProvider } from "./context/settingsContext";
 import axios from "axios";
 import Home from "./components/Home";
 import DogDetails from "./components/DogDetails";
+import NotFound from "./components/NotFound";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 const router = createBrowserRouter([
     {
-      path: "/",
-      element: <App />,
-      children: [
-        {
-          index: true,
-          element: <Home />,
-          loader: async () => {
-            const [{ data: dogs }, { data: user }] = await axios.all([
-              axios.get(`${import.meta.env.VITE_API_ENDPOINT}/dogs`),
-              axios.get(`${import.meta.env.VITE_API_ENDPOINT}/user`)
-            ]);
-  
-            return { dogs, user };
-          }
-        }
-      ]
+        HydrateFallback: App,
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+                ErrorBoundary: ErrorBoundary,
+                loader: async () => {
+                    const [{ data: dogs }, { data: user }] = await axios.all([
+                        axios.get(`${import.meta.env.VITE_API_ENDPOINT}/dogs`),
+                        axios.get(`${import.meta.env.VITE_API_ENDPOINT}/user`)
+                    ]);
+
+                    return { dogs, user };
+                }
+            }
+        ]
     },
     {
-      path: "/dog/:id",
-      element: <DogDetails />,
-      loader: async ({ params }) => {
-        const { id } = params;
-        const { data: dog } = await axios.get<Pet>(
-            `${import.meta.env.VITE_API_ENDPOINT}/dogs/${id}`
-        )
+        HydrateFallback: App,
+        path: "/dog/:id",
+        element: <DogDetails />,
+        ErrorBoundary: ErrorBoundary,
+        loader: async ({ params }) => {
+            const { id } = params;
+            const { data: dog } = await axios.get<Pet>(
+                `${import.meta.env.VITE_API_ENDPOINT}/dogs/${id}`
+            )
 
-        return { dog };
-      }
+            return { dog };
+        }
+    },
+    {
+        path: "*",
+        element: <NotFound />
     }
-  ]);
+]);
 
 const root = document.getElementById("root") as HTMLElement;
 
